814799ffde28c910583b251e6ece4924062ebbff
huangxiang <huangxiang1992@hotmail.com>
2018-09-08 03:47:47 +0800

    1. 添加博客全站搜索 2. 更换sqlite为postgresql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'taggit',
     'django.contrib.sites',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
+    'django.contrib.postgres',
     ]
 
 MIDDLEWARE = [
@@ -77,10 +78,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'huangxiang',
+        'PASSWORD': 'hx15502040571.'
     }
 }
 
